5b4403ff30dfdd9f4c923b5642094f967b182e29
pritom352 <pritomproshad@gmail.com>
2025-08-22 15:49:11 +0600

    update

--- app/page.js
+++ app/page.js
@@ -6,7 +6,7 @@ import ProductHighlights from "./components/ProductHighlights";
 export default async function HomePage() {
   const client = await clientPromise;
   const db = client.db("techgadgets"); // তোমার ডাটাবেস নাম
-  const products = await db.collection("products").find({}).toArray();
+  const products = await db.collection("products").find({}).limit(8).toArray();
 
   return (
     <main>
